fd48980d69bb25260b7b6d240175e7687320c427
Michael Heiniger <michael.heiniger@nzz.ch>
2012-02-05 23:17:49 +0100

    added routes and welcome screen

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Web Song Presenter' })
+};
+
+exports.view = function(req, res){
+  res.render('view', { title: 'Web Song Presenter' })
+};
+
+exports.control = function(req, res){
+    res.render('control', { title: 'Web Song Presenter' })
 };
